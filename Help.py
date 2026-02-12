003cf7807af17c648c4d88713d8e6ca9f8ff24e3
rebornloki-ops <lydieakokpe431@gmail.com>
2026-02-12 05:36:21 +0100

    Add files via upload

---
+++ Help.py
@@ -0,0 +1,9 @@
+name = "Jerry"
+age= 12
+print(name)
+print(age)
+print("My name is",name,"and i am",age) 
+name = input("What is your name")
+print("welcome",name)
+favgame = input ("Whats your favorite game")
+print("Oh so you like",favgame,"!","Good Choice!")
