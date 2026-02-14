ad20d18ea5607c367b8b0aa1646492d9a2a4b1b3
rebornloki-ops <lydieakokpe431@gmail.com>
2026-02-14 12:52:20 +0100

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-name = "Jerry"
-age = 12
-print("Hello my name is",name,"I am",age)
-username = input ("Whats your name")
-print("Welcome",username)
-favgame = input ("whats your favorite game")
-print("Oh you like",favgame,"Good Choice !")
